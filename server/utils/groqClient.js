b04d15825b5691f3bd2c2589ad7054f8d452b8ef
ArpitUniyal <arpituniyal12@gmail.com>
2026-01-17 00:30:06 +0530

    Update groqClient.js

--- server/utils/groqClient.js
+++ server/utils/groqClient.js
@@ -4,4 +4,8 @@ const groq = new Groq({
   apiKey: process.env.GROQ_API_KEY
 });
 
+console.log("GROQ_API_KEY at runtime:", process.env.GROQ_API_KEY ? "FOUND" : "MISSING");
+
+
 module.exports = groq;
+
