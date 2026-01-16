39757c5006cee7ac142d88a2497e4582c17ec8fd
ArpitUniyal <arpituniyal12@gmail.com>
2026-01-17 00:29:41 +0530

    Update listGroqModel.js

--- server/listGroqModel.js
+++ server/listGroqModel.js
@@ -4,6 +4,8 @@ const Groq = require("groq-sdk");
 const groq = new Groq({
   apiKey: process.env.GROQ_API_KEY
 });
+console.log("GROQ_API_KEY at runtime:", process.env.GROQ_API_KEY ? "FOUND" : "MISSING");
+
 
 (async () => {
   try {
@@ -16,3 +18,4 @@ const groq = new Groq({
     console.error("Error fetching models:", err.message);
   }
 })();
+
