0c3674d99d1bb2bc4ebadc7611492de3f02f58d9
ArpitUniyal <arpituniyal12@gmail.com>
2026-08-23 16:14:04 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider, useAuth } from "./context/AuthContext";
 import ProtectedRoute from "./components/common/ProtectedRoute";
 
 import Login from "./pages/Login";
@@ -7,18 +7,39 @@ import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import PdfDetails from "./pages/PdfDetails";
 import Home from "./pages/Home";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
 
+function RootRedirect() {
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return null;
+  }
+
+  return user
+    ? <Navigate to="/dashboard" replace />
+    : <Home />;
+}
 
 function App() {
   return (
     <AuthProvider>
       <Router>
         <Routes>
-          {/* DEFAULT ROUTE */}
-         {/* <Route path="/" element={<Navigate to="/login" replace />} />*/}
-          <Route path="/" element={<Home />} />
+
+          <Route path="/" element={<RootRedirect />} />
+
           <Route path="/login" element={<Login />} />
+
           <Route path="/register" element={<Register />} />
+
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+
+          <Route
+  path="/reset-password"
+  element={<ResetPassword />}
+/>
 
           <Route
             path="/dashboard"
@@ -37,6 +58,7 @@ function App() {
               </ProtectedRoute>
             }
           />
+
         </Routes>
       </Router>
     </AuthProvider>
@@ -44,4 +66,3 @@ function App() {
 }
 
 export default App;
-
