8d4dfcf304ffb3d345b055f8a9ef9fe00e56172a
ArpitUniyal <arpituniyal12@gmail.com>
2026-09-05 20:32:12 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -19,10 +19,8 @@ const logger = winston.createLogger({
     winston.format.json()
   ),
   transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' })
-  ]
+  new winston.transports.Console()
+]
 });
 
 // Middleware
