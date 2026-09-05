34036e5add14ded2cdaba337c4885caae2121316
ArpitUniyal <arpituniyal12@gmail.com>
2026-09-05 17:34:19 +0530

    Update pdf.js

--- server/routes/pdf.js
+++ server/routes/pdf.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
 const pdfParse = require('pdf-parse');
 const { auth } = require('../middleware/auth');
 const { Pdf } = require('../models');
