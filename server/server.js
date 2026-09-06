02d587bd2992cd5d42e0379bc991c033089d2a8d
ArpitUniyal <arpituniyal12@gmail.com>
2026-09-06 10:44:01 +0530

    Refactor CORS configuration for dynamic origin handling

--- server/server.js
+++ server/server.js
@@ -30,19 +30,18 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: allowedOrigin,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-
-app.options("*", cors({
-  origin: allowedOrigin,
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
